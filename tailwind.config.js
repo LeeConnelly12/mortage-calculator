dbe17a95737f5ebd17908a128d1c9de936230731
Lee Connelly <lee@leeconnelly.dev>
2024-06-28 23:07:47 +0100

    Add font

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,14 @@
-import forms from "@tailwindcss/forms";
+import defaultTheme from 'tailwindcss/defaultTheme'
+import forms from '@tailwindcss/forms'
 
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Plus Jakarta Sans', ...defaultTheme.fontFamily.sans],
+    },
     extend: {},
   },
   plugins: [forms],
-};
+}
