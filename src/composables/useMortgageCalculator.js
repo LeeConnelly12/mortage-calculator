54c848b9db6326e84274a49b2a5f9161272a1ef9
Lee Connelly <lee@leeconnelly.dev>
2024-06-29 01:31:35 +0100

    Handle interest only

--- src/composables/useMortgageCalculator.js
+++ src/composables/useMortgageCalculator.js
@@ -1,4 +1,3 @@
-// src/composables/useMortgageCalculator.js
 import { ref } from 'vue'
 
 export function useMortgageCalculator() {
@@ -12,13 +11,19 @@ export function useMortgageCalculator() {
     }).format(value)
   }
 
-  function calculateMortgage(amount, term, interestRate) {
+  function calculateMortgage(amount, term, interestRate, type) {
     const monthlyInterestRate = interestRate / 100 / 12
     const numberOfPayments = term * 12
 
-    const payment =
-      (amount * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))) /
-      (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1)
+    let payment
+
+    if (type === 'repayment') {
+      payment =
+        (amount * (monthlyInterestRate * Math.pow(1 + monthlyInterestRate, numberOfPayments))) /
+        (Math.pow(1 + monthlyInterestRate, numberOfPayments) - 1)
+    } else if (type === 'interestOnly') {
+      payment = amount * monthlyInterestRate
+    }
 
     monthlyPayment.value = formatCurrency(payment)
     totalPayment.value = formatCurrency(payment * numberOfPayments)
